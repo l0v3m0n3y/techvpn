472a1a63b5bffb16eba9e40e2b4bccbde4143987
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2026-08-25 13:25:11 +0300

    Update techvpn.swift

--- src/techvpn.swift
+++ src/techvpn.swift
@@ -34,16 +34,27 @@ public class Techvpn{
         ]
 
     }
-
-    public func get_proxy_list() async throws -> Any {
-        let urlString = "\(api)/extension/servers"
-        guard let url = URL(string: urlString) else {
+    
+    private func fetchJSON(from urlString: String,method: HTTPMethod = .get,body: Data? = nil,queryParameters: [String: String]? = nil) async throws -> Any {
+        var urlComponents = URLComponents(string: urlString)
+        if let queryParameters = queryParameters {
+            urlComponents?.queryItems = queryParameters.map { URLQueryItem(name: $0.key, value: $0.value) }
+        }
+        guard let url = urlComponents?.url else {
             throw NSError(domain: "Invalid URL", code: -1)
         }
         var request = URLRequest(url: url)
-        request.httpMethod = "GET"
+        request.httpMethod = method.rawValue
         request.allHTTPHeaderFields = headers
+        if let body = body {
+            request.httpBody = body
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        }
         let (data, _) = try await URLSession.shared.data(for: request)
         return try JSONSerialization.jsonObject(with: data)
+    }
+
+    public func getProxyList() async throws -> Any {
+        return try await fetchJSON(from: "\(api)/extension/servers")
     }
 }
